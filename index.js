dd79692306cb28c61eddd417c83db39104ad3bb3
joaovsilva18 <joaovactorsilva@gmail.com>
2022-10-19 16:18:21 -0300

    aula 01

---
+++ index.js
@@ -0,0 +1,8 @@
+let segundaforma = document.querySelector('#event')
+
+
+segundaforma.addEventListener("click", (e) => mostrarconsole(e));
+
+function mostrarconsole(e) {
+    console.log("Ola mundo");
+}
